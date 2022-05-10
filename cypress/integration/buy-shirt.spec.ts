2cd16cde171b95fb5f700ac1bc73b8aea58d0345
Fredy Arroyave <fredy.arroyave@udea.edu.co>
2022-05-10 15:25:23 -0500

    comments fixed

--- cypress/integration/buy-shirt.spec.ts
+++ cypress/integration/buy-shirt.spec.ts
@@ -8,15 +8,26 @@ import {
   ShippingPage,
   ShoppingCartPage} from "../page/index";
 
-const menuContentPage = new MenuContentPage();
-const productListPage = new ProductListPage();
-const shoppingCartPage = new ShoppingCartPage();
-const loginPage = new LoginPage();
-const addressStepPage = new AddressStepPage();
-const shippingPage = new ShippingPage();
-const paymentPage = new PaymentPage();
 
 describe("Buy a t-shirt", () => {
+  let menuContentPage:MenuContentPage;
+  let productListPage:ProductListPage;
+  let shoppingCartPage:ShoppingCartPage;
+  let loginPage:LoginPage;
+  let addressStepPage:AddressStepPage;
+  let shippingPage:ShippingPage;
+  let paymentPage:PaymentPage;
+
+  before(()=> {
+    menuContentPage = new MenuContentPage();
+    productListPage = new ProductListPage();
+    shoppingCartPage = new ShoppingCartPage();
+    loginPage = new LoginPage();
+    addressStepPage = new AddressStepPage();
+    shippingPage = new ShippingPage();
+    paymentPage = new PaymentPage();
+  });
+
   it("then the t-shirt should be bought", () => {
     // arrange
     menuContentPage.visitMenuContentPage();
--- cypress/integration/dresses-list.spec.ts
+++ cypress/integration/dresses-list.spec.ts
@@ -3,16 +3,17 @@ import {MenuContentPage, DressesListPage} from "../page/index";
 describe("the user navigates to the dresses page should", () => {
   let menuContentPage: MenuContentPage;
   let dressesListPage: DressesListPage;
+  let allDresses: string[];
 
   before(() => {
     menuContentPage = new MenuContentPage();
     dressesListPage = new DressesListPage();
+    allDresses =
+    ["Printed Dress", "Printed Dress", "Printed Summer Dress", "Printed Summer Dress", "Printed Chiffon Dress"];
   });
 
   it("show the available dresses", () => {
     // arrange
-    const allDresses =
-    ["Printed Dress", "Printed Dress", "Printed Summer Dress", "Printed Summer Dress", "Printed Chiffon Dress"];
     menuContentPage.visitMenuContentPage();
 
     // action
